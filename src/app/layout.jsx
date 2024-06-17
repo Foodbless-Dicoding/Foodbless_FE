a671a8d096f07cb8804b8092ea78035f4a5868c8
unknown <rhesakornelius@gmail.com>
2024-06-17 07:18:43 +0700

    feat: fixed meta #1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import PrelineScript from "@/components/PrelineScript";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,13 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta charSet="utf-8" />
+      <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="dicoding:email" content="rhesakornelius@gmail.com" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-      </Head>
+      </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         {children}
         <PrelineScript />
